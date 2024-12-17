46d6f30d8b45067d0fc006186d957918b18089af
AdityaSujith <adityasujith1226@gmail.com>
2024-12-17 01:22:08 +0000

    commit Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
 import java.util.*;
+
 /**
- * Driver class, sets up game
- * Look to comments to see what you need to do to test your game
+ * Driver class, sets up game.
+ * Look to comments to see what you need to do to test your game.
  */
-public class Main
-{
-    public static void main(String [] args)
-    {
-        Player [] players = new Player[2];  //need to update array based on number of players
-        boolean printsRun = false;   //printsRun is a boolean set to display moves as game goes, used for debugging
-        
-        //add players to game
-        players[0] = new Example1();
-        players[1] = new PlayerExample();
-   
-        //construct the game with the classes in the players array, calls on play and displays the score
-        //DO NOT CHANGE
+public class Main {
+    public static void main(String[] args) {
+        Player[] players = new Player[2]; // Update array based on number of players
+        boolean printsRun = false;        // Boolean to display moves as game goes, used for debugging
+
+        // Add players to the game
+        players[0] = new CopycatPlayer("Copycat");
+        players[1] = new RandomPlayer("Random");
+
+        // Construct the game with the players array, calls play, and displays the score
+        // DO NOT CHANGE
         Game newGame = new Game(players, printsRun);
         newGame.play();
         newGame.displayScore();
--- Player.java
+++ Player.java
@@ -1,12 +1,39 @@
-import java.util.*;
-/**
- * Write a description of interface Player here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public interface Player
-{
-    public String move(String [] myMoves, String [] opponentMoves, int myScore, int opponentScore);
-    public String getName();
+import java.util.Random;
+
+public class CopycatPlayer implements Player {
+    private String name;
+    private Random random;
+    private String nextMove; // Stores the move for the next round
+
+    // Constructor to set the player's name
+    public CopycatPlayer(String name) {
+        this.name = name;
+        this.random = new Random();
+        this.nextMove = getRandomMove(); // Initial move is random
+    }
+
+    @Override
+    public String move(String[] myMoves, String[] opponentMoves, int myScore, int opponentScore) {
+        return nextMove;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    // Method to set the next move if the player wins
+    public void setNextMove(String opponentMove, boolean didWin) {
+        if (didWin) {
+            nextMove = opponentMove; // Copy opponent's move if the player wins
+        } else {
+            nextMove = getRandomMove(); // Otherwise, choose a random move
+        }
+    }
+
+    // Helper method to generate a random move
+    private String getRandomMove() {
+        String[] moves = {"rock", "paper", "scissors"};
+        return moves[random.nextInt(moves.length)];
+    }
 }
